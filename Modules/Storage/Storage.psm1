a822d122ecb52aa6d9d50b2e1ea20de1264aa649
Daniel Lerch <daniel.p.lerch@gmail.com>
2020-09-21 15:15:20 +0200

    Improve disk erase tool UI

--- Modules/Storage/Storage.psm1
+++ Modules/Storage/Storage.psm1
@@ -1,12 +1,17 @@
 <#
 .SYNOPSIS
-Overrides a physical device with random data
+	Overrides a physical device with random data
+.PARAMETER Number
+	The physical drive number to override
+.PARAMETER Force
+	Start overriding without prompting for confirmation
 #>
 function Clear-PhysicalDrive {
-	[CmdletBinding()]
+	[CmdletBinding(SupportsShouldProcess = $true)]
 	param (
 		[Parameter(Mandatory = $true, Position = 0)]
-		[int]$Number
+		[int]$Number,
+		[switch]$Force
 	)
 	
 	begin {
@@ -15,36 +20,51 @@ function Clear-PhysicalDrive {
 				return "$bytes bytes"
 			}
 			elseif ($bytes -lt 1024 * 1024) {
-				return "$($bytes / 1024) KB"
+				return "$([System.Math]::Round($bytes / 1024)) KB"
 			}
 			elseif ($bytes -lt 1024 * 1024 * 1024) {
-				return "$($bytes / 1024 / 1024) MB"
+				return "$([System.Math]::Round($bytes / 1024 / 1024, 1)) MB"
 			}
 			else {
-				return "$($bytes / 1024 / 1024 / 1024) GB"
+				return "$([System.Math]::Round($bytes / 1024 / 1024 / 1024, 2)) GB"
 			}
 		}
 	}
 	
 	process {
-		$buffer = New-Object byte[] 16777216
+		$bufferSizeMb = 32
+		$buffer = New-Object byte[] ($bufferSizeMb * 1048576)
 		$random = New-Object System.Random
 		[long]$done = 0
 
 		try {
-			$stream = New-Object System.IO.FileStream("\\.\PHYSICALDRIVE$Number", [System.IO.FileMode]::Open, [System.IO.FileAccess]::Write, [System.IO.FileShare]::None, $buffer.Length, [System.IO.FileOptions]::None)
+			$diskDrive = Get-CimInstance -ClassName "Win32_DiskDrive" -Filter "DeviceID LIKE '%PHYSICALDRIVE$Number'"
+			$displayName = "$($diskDrive.Caption) [$(FormatBytes $diskDrive.Size)]"
+			if (!($PSCmdlet.ShouldProcess($displayName, "override"))) {
+				$done = $diskDrive.Size
+				return
+			}
+			if (!($Force -or $PSCmdlet.ShouldContinue("Do you really want to override $($displayName)?", "Override?"))) {
+				return
+			}
+			$stream = New-Object System.IO.FileStream("\\.\PHYSICALDRIVE$Number", [System.IO.FileMode]::Open, [System.IO.FileAccess]::ReadWrite, [System.IO.FileShare]::None, $buffer.Length, [System.IO.FileOptions]::None)
+			$stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 			while ($true) {
 				$random.NextBytes($buffer)
 				$stream.Write($buffer, 0, $buffer.Length)
 				$done += $buffer.Length
-				Write-Progress -Activity "Overriding drive $Number" -Status "$(FormatBytes $done) written"
+				[int]$percentComplete = [System.Math]::Round($done / $diskDrive.Size)
+				$stopwatch.Stop()
+				[long]$bytesPerSecond = $buffer.Length / $stopwatch.Elapsed.TotalSeconds
+				$stopwatch.Restart()
+				Write-Progress -Activity "Overriding $displayName" -Status "$(FormatBytes $done) written ($(FormatBytes $bytesPerSecond)/s)" -PercentComplete $percentComplete
 			}
 		}
 		finally {
 			if ($stream) {
 				$stream.Dispose()
 			}
-			Write-Progress -Activity "Overriding drive $Number" -Completed
+			Write-Progress -Activity "Overriding $displayName" -Completed
 			Write-Host "$(FormatBytes $done) written"
 		}
 	}
