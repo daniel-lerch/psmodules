6bebf626953776fee510cee83a7b47c6263efdcf
Daniel Lerch <daniel.p.lerch@gmail.com>
2021-10-21 22:44:10 +0200

    Improve database server setup - Change location to omit version numbers in paths - Implement data migration on upgrade - Update to MySQL 5.7.35 and MariaDB 10.6.4

--- Modules/Database/Database.psm1
+++ Modules/Database/Database.psm1
@@ -1,6 +1,6 @@
-$mysqlName = "mysql-5.7.30-winx64"
-$mariadbName = "mariadb-10.5.5-winx64"
-$installPath = Join-Path $env:LOCALAPPDATA "Programs" "Database Servers"
+$mysqlName = "MySQL"
+$mariadbName = "MariaDB"
+$installPath = Join-Path $env:LOCALAPPDATA "Programs"
 
 function Install-DbServer {
 	[CmdletBinding()]
@@ -11,86 +11,102 @@ function Install-DbServer {
 		[switch]$MariaDB
 	)
 
-	begin {
-		# We require mirrors to support HTTPS
-		$mysqlMirror = "https://cdn.mysql.com//Downloads/MySQL-5.7/mysql-5.7.30-winx64.zip"
-		$mysqlSize = "368 MB"
-		$mariadbMirror = "https://mirrors.ukfast.co.uk/sites/mariadb//mariadb-10.5.5/winx64-packages/mariadb-10.5.5-winx64.zip"
-		$mariadbSize = "68 MB"
+	# We require mirrors to support HTTPS
+	$mysqlMirror = "https://downloads.mysql.com/archives/get/p/23/file/mysql-5.7.35-winx64.zip"
+	$mysqlSize = "370 MB"
+	$mariadbMirror = "https://downloads.mariadb.org/rest-api/mariadb/10.6.4/mariadb-10.6.4-winx64.zip"
+	$mariadbSize = "71 MB"
 
-		function InstallDbServer ($Mirror, $Size, $Name) {
-			$guid = [System.Guid]::NewGuid().ToString()
-			$tempPath = Join-Path $env:TEMP $guid
-			$downloadPath = Join-Path $env:TEMP ($guid + ".zip")
-			$versionPath = Join-Path $installPath $Name
+	function InstallDbServer ($Mirror, $Size, $Name) {
+		$guid = [System.Guid]::NewGuid().ToString()
+		$tempPath = Join-Path $env:TEMP $guid
+		$downloadPath = Join-Path $env:TEMP ($guid + ".zip")
+		$versionPath = Join-Path $installPath $Name
 
-			$message = "Download and install?"
-			$question = "The download size is $Size. Do you want to proceed?"
+		$message = "Download and install?"
+		$question = "The download size is $Size. Do you want to proceed?"
+		$choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+		$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList "&Yes"))
+		$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList "&No"))
+		
+		$decision = $Host.UI.PromptForChoice($message, $question, $choices, 1)
+		if ($decision -eq 1) {
+			return
+		}
+			
+		Write-Host "Downloading archive..."
+		Start-BitsTransfer -Source $Mirror -Destination $downloadPath -ErrorAction Stop
+		Write-Host "Unpacking archive..."
+		Expand-Archive -Path $downloadPath -DestinationPath $tempPath
+		$innerTempFolder = Get-ChildItem -Path $tempPath -Directory
+		if (Test-Path -Path $versionPath) {
+			if (Test-Path -Path (Join-Path $versionPath "data")) {
+				Write-Host "Migrating data..."
+				Move-Item -Path (Join-Path $versionPath "data") -Destination $innerTempFolder.FullName
+			}
+			Write-Host "Removing old files..."
+			Remove-Item -Path $versionPath -Recurse
+		}
+		Move-Item -Path $innerTempFolder.FullName -Destination $versionPath -Force
+
+		Write-Host "Cleaning up..."
+		# Continue silenty in order not rely on temporary files still being in place
+		Remove-Item -Path $downloadPath -ErrorAction SilentlyContinue
+		Remove-Item -Path $tempPath -ErrorAction SilentlyContinue
+	}
+
+	function PurgeDbServer ($Path) {
+		if (Test-Path -Path $Path -PathType Container) {
+			$message = "Remove old database server?"
+			$question = "An outdated database server was found at $Path. Do you want to remove it?"
 			$choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
 			$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList "&Yes"))
 			$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList "&No"))
-		
+			
 			$decision = $Host.UI.PromptForChoice($message, $question, $choices, 1)
-			if ($decision -eq 1) {
-				return
-			}
-			
-			Write-Host "Downloading archive..."
-			Start-BitsTransfer -Source $Mirror -Destination $downloadPath -ErrorAction Stop
-			Write-Host "Unpacking archive..."
-			Expand-Archive -Path $downloadPath -DestinationPath $tempPath
-			if (!(Test-Path -PathType Container -Path $installPath)) { New-Item -ItemType Directory -Force -Path $installPath | Out-Null }
-			if (Test-Path -Path $versionPath) {	Remove-Item -Path $versionPath -Recurse -Force }
-			Move-Item -Path (Join-Path $tempPath $Name) -Destination $versionPath -Force
-
-			Write-Host "Cleaning up..."
-			# Do not rely on temporary files not to be deleted in the meantime
-			Remove-Item -Path $downloadPath -ErrorAction SilentlyContinue
-			Remove-Item -Path $tempPath -ErrorAction SilentlyContinue
-		}
-
-		function PurgeDbServer ($Path) {
-			if (Test-Path -Path $Path -PathType Container) {
-				$message = "Remove old database server?"
-				$question = "An outdated database server was found at $Path. Do you want to remove it?"
-				$choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
-				$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList "&Yes"))
-				$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList "&No"))
-			
-				$decision = $Host.UI.PromptForChoice($message, $question, $choices, 1)
-				if ($decision -eq 0) {
-					Remove-Item -Path $Path -Recurse
-				}
+			if ($decision -eq 0) {
+				Remove-Item -Path $Path -Recurse
 			}
 		}
 	}
-	process {
-		$mysqlInstalled = Test-Path -Path (Join-Path $installPath $mysqlName)
-		$mariadbInstalled = Test-Path -Path (Join-Path $installPath $mariadbName)
-		Write-Host "Installed database servers:"
-		if ($mysqlInstalled) {
-			Write-Host "MySQL:    $mysqlName"
-		} else {
-			Write-Host "MySQL:    ---"
-		}
-		if ($mariadbInstalled) {
-			Write-Host "MariaDB:  $mariadbName"
-		} else {
-			Write-Host "MariaDB:  ---"
-		}
-		Write-Host ""
-		Write-Host "Already installed versions will be overridden"
-		Write-Host ""
 
-		if ($MySQL) {
-			InstallDbServer -Mirror $mysqlMirror -Size $mysqlSize -Name $mysqlName
+	function PurgeEmptyFolder ($Path) {
+		$info = (Get-Item -Path $Path -ErrorAction SilentlyContinue)
+		if ($info.Exists -and $info.GetFiles().Count -eq 0) {
+			Remove-Item -Path $Path -Recurse
 		}
-		if ($MariaDB) {
-			InstallDbServer -Mirror $mariadbMirror -Size $mariadbSize -Name $mariaName
-		}
-
-		PurgeDbServer -Path (Join-Path $env:LOCALAPPDATA MariaDB)
 	}
+
+	$mysqlInstalled = Test-Path -Path (Join-Path $installPath $mysqlName)
+	$mariadbInstalled = Test-Path -Path (Join-Path $installPath $mariadbName)
+	Write-Host "Installed database servers:"
+	if ($mysqlInstalled) {
+		Write-Host "MySQL:    $mysqlName"
+	}
+	else {
+		Write-Host "MySQL:    ---"
+	}
+	if ($mariadbInstalled) {
+		Write-Host "MariaDB:  $mariadbName"
+	}
+	else {
+		Write-Host "MariaDB:  ---"
+	}
+	Write-Host ""
+	Write-Host "Already installed versions will be overridden"
+	Write-Host ""
+
+	if ($MySQL) {
+		InstallDbServer -Mirror $mysqlMirror -Size $mysqlSize -Name $mysqlName
+	}
+	if ($MariaDB) {
+		InstallDbServer -Mirror $mariadbMirror -Size $mariadbSize -Name $mariadbName
+	}
+
+	PurgeDbServer (Join-Path $env:LOCALAPPDATA MariaDB)
+	PurgeDbServer (Join-Path $env:LOCALAPPDATA "Programs" "Database Servers" "mysql-5.7.30-winx64")
+	PurgeDbServer (Join-Path $env:LOCALAPPDATA "Programs" "Database Servers" "mariadb-10.5.5-winx64")
+	PurgeEmptyFolder (Join-Path $env:LOCALAPPDATA "Programs" "Database Servers")
 }
 
 function Start-DbServer {
@@ -117,18 +133,19 @@ function Start-DbServer {
 			$firstRun = !(Test-Path -Path (Join-Path (Join-Path $installPath $mysqlName) "data") -PathType Container)
 			if ($firstRun) {
 				Start-Process -FilePath $serverPath -ArgumentList `
-					"--console","--skip-log-syslog","--explicit-defaults-for-timestamp", `
+					"--console", "--skip-log-syslog", "--explicit-defaults-for-timestamp", `
 					"--initialize-insecure" -Wait
 			}
 			Start-Process -FilePath $serverPath -ArgumentList `
-				"--console","--skip-log-syslog","--explicit-defaults-for-timestamp","--transaction-isolation=READ-COMMITTED"
+				"--console", "--skip-log-syslog", "--explicit-defaults-for-timestamp", "--transaction-isolation=READ-COMMITTED"
 			if ($firstRun) {
 				$execPath = GetPathWhenRunning -Name "mysqladmin.exe"
 				if ($execPath) {
-					Start-Process -FilePath $execPath -ArgumentList "-uroot","password root"
+					Start-Process -FilePath $execPath -ArgumentList "-uroot", "password root"
 				}
 			}
-		} else {
+		}
+		else {
 			Write-Host "MySQL was not found at $serverPath"
 		}
 	}
@@ -143,8 +160,9 @@ function Start-DbServer {
 					"--password=root" -Wait
 			}
 			Start-Process -FilePath $serverPath -ArgumentList `
-				"--console","--transaction-isolation=READ-COMMITTED"
-		} else {
+				"--console", "--transaction-isolation=READ-COMMITTED"
+		}
+		else {
 			Write-Host "MariaDB was not found $serverPath"
 		}
 	}
@@ -153,7 +171,7 @@ function Start-DbServer {
 function Stop-DbServer {
 	$execPath = GetPathWhenRunning -Name "mysqladmin.exe"
 	if ($execPath) {
-		Start-Process -FilePath $execPath -ArgumentList "-uroot","-proot","shutdown"
+		Start-Process -FilePath $execPath -ArgumentList "-uroot", "-proot", "shutdown"
 	}
 }
 
@@ -179,10 +197,12 @@ function GetPathWhenRunning ($Name) {
 		$execPath = Join-Path $binPath $Name
 		if (Test-Path -Path $execPath -PathType Leaf) {
 			return $execPath
-		} else {
+		}
+		else {
 			"Could not find executable at $execPath"
 		}
-	} else {
+	}
+	else {
 		Write-Host "No MySQL/MariaDB server is currently running on this system."
 	}
 }
